353c32ea67c367c23de365fbe87bf77eed66d343
kamilo9809 <camilorivera09@outlook.com>
2024-10-02 21:37:17 -0500

    first commit

---
+++ src/main/java/com/lrosania/springBackend_example/SpringBackendExampleApplication.java
@@ -0,0 +1,13 @@
+package com.lrosania.springBackend_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lrosania/springBackend_example/SpringBackendExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrosania.springBackend_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
